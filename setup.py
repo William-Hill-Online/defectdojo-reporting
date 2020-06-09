762a0c4806f5c31b05ec6e4d73bc0803e6505a36
Ederson Brilhante <ederson.brilhante@grandparade.co.uk>
2020-06-09 17:42:22 +0200

    feature: add files to build client

---
+++ setup.py
@@ -0,0 +1,46 @@
+# Copyright 2019 WHG (International) Limited. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+import json
+
+from setuptools import find_packages
+from setuptools import setup
+
+NAME = 'defectdojo-reporting'
+VERSION = '1.0.0'
+
+install_requires = []
+tests_require = []
+
+with open('Pipfile.lock') as fd:
+    lock_data = json.load(fd)
+    install_requires = [
+        package_name + package_data['version']
+        for package_name, package_data in lock_data['default'].items()
+    ]
+    tests_require = [
+        package_name + package_data['version']
+        for package_name, package_data in lock_data['develop'].items()
+    ]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description='CCVS API Client',
+    author='Ederson Brilhante',
+    author_email='ederson.brilhante@grandparade.co.uk',
+    url='https://github.com/William-Hill-Online/CCVS-API-Client',
+    keywords=['CCVS API'],
+    install_requires=install_requires,
+    tests_require=tests_require,
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'dd-reporting=defectdojo_reporting.reporting:Main',
+        ],
+    },
+    long_description="""\
+        Client for sending reports to DefectDojo
+    """
+)
